fe077ebde1c06b374b0d425dfdf97220b6e9da01
baliram-dev <baliramchauhan959874@gmail.com>
2026-04-28 13:28:06 +0100

    Backend testing solved

--- emergency-lifeline/backend/app.py
+++ emergency-lifeline/backend/app.py
@@ -654,8 +654,9 @@ def admin_upload_blueprint():
             cursor.close()
             conn.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
 @app.route("/")
 def home():
     return "SafePath Backend Running 🚀"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)
